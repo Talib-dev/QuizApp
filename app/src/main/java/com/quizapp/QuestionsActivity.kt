63e64d1553a20a37ac13b68602b696527052f1ee
mohdTalibAhmed <mohdtalib18799@gmail.com>
2021-02-10 17:37:19 +0530

    initialize

--- app/src/main/java/com/quizapp/QuestionsActivity.kt
+++ app/src/main/java/com/quizapp/QuestionsActivity.kt
@@ -3,14 +3,27 @@ package com.quizapp
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_questions.*
 
 class QuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val TAG = "QuestionsActivity"
+        val currentPosition=1
 
         setContentView(R.layout.activity_questions)
         val questionsList = Constent.getQuestions()
         Log.d(TAG, "${questionsList.size}")
+        val question:Question=questionsList[currentPosition-1]
+
+        tv_question.text=question.question
+        iv_image.setImageResource(question.image)
+        progressBar.progress=currentPosition
+        tv_progress.text= "$currentPosition/10"
+        tv_option_one.text =question.optionFirst
+        tv_option_two.text =question.optionSecond
+        tv_option_three.text = question.optionThird
+        tv_option_four.text = question.optionFourth
+
     }
 }
